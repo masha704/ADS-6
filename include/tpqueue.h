e79d99b3812709546c43c23ac971e32ac32d310c
Maria Anikina <59134557+masha704@users.noreply.github.com>
2022-04-16 22:18:21 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,27 +4,27 @@
 
 template<typename T, int size>
 class TPQueue {
-  private:
-      int first, last;
-      T arr[size];
-  public:
-      TPQueue: first(0), last(0) {}
-      T pop() {
-          return arr[(first++)%size];
+ private:
+    int first, last;
+    T arr[size];
+ public:
+    TPQueue: first(0), last(0) { }
+    T pop() {
+      return arr[(first++)%size];
+    }
+    void push(T value) {
+      int tmp = last;
+      bool count = false;
+      while(!count) {
+        if((first < --tmp) && (value.prior > arr[last%size].prior)) {
+          arr[(tmp+1)%size] = arr[tmp%size];
+        } else {
+            count = true;
+          }
+        arr[(tmp+1)%size] = value;
+        last ++;
       }
-      void push(T value) {
-          int tmp = last;
-          bool count = false;
-          while(!count) {
-              if((first < --tmp) && (value.prior > arr[last%size].prior)) {
-                  arr[(tmp+1)%size] = arr[tmp%size];
-              } else {
-                  count = true;
-              }
-              arr[(tmp+1)%size] = value;
-              last ++;
-         }
-     }
+    }
   // реализация шаблона очереди с приоритетом на кольцевом буфере
 };
 
